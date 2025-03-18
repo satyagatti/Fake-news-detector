fec1c9d84f683439b899a3798e8fc344748f453a
Satya Chaitanya <satyachaitanyagatti@gmail.com>
2025-03-18 21:13:31 +0530

    adding requirement.txt

--- app.py
+++ app.py
@@ -22,5 +22,9 @@ def predict():
 
     return render_template("index.html", prediction=result)
 
+def handler(event, context):
+    return app(event, context)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
